9c8a77ab55db17f0de59daeeb6bb31f4c5c59536
Abid Ibn Habib <34665203+Abid0Hasan@users.noreply.github.com>
2020-03-28 20:57:39 +0600

    Add files via upload

---
+++ box/index.php
@@ -0,0 +1,73 @@
+<?php
+include_once"classes/Shout.php";
+$shout = new Shout();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Shout Box</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="wrapper clr">
+     <header class="headsection clr"> 
+     	<h2>Basic Shoutbox with PHP OOP & MySQL</h2>
+     </header>
+
+
+
+		<section class="content clr">
+			<div class="box">
+				<ul>
+                    <?php 
+                         $getData = $shout->getAllData();
+                         if ($getData) {
+                         	while ($data = $getData->fetch_assoc()) { ?>
+
+                         		<li><span><?php echo $data['time'] ?></span>- <b><?php echo $data['name'] ?></b> <?php echo $data['body'] ?></li>
+
+                         	<?php } } ?>
+				</ul>
+			 </div>
+
+
+			 <?php 
+               
+               if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+               	$shoutdata = $shout->insertData($_POST);
+               }
+
+			 ?>
+
+			<div class="shoutform clr">
+				<form action="" method="post">
+					<table>
+						<tr>
+							<td>Name</td>
+							<td>:</td>
+							<td><input type="text" name="name" required placeholder="Enter Your name..."/></td>
+						</tr>
+						<tr>
+							<td>Body</td>
+							<td>:</td>
+							<td><input type="text" name="body" required placeholder="Enter Your text..." /></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td><input type="submit" value="Shout It" /></td>
+						</tr>
+					</table>
+				</form>
+			</div>
+		</section>
+
+        <footer class="footsection clr">
+            <h2>&copy: Copyright abidhasan.xyz</h2>        	
+        </footer>
+
+	</div>
+
+</body>
+</html>
